3ea1addd74264d476b71eac803042e82c5d8afd1
Vipul Chauhan <viratdev07@gmail.com>
2021-01-01 09:39:47 +0530

    Add PasswordEncoder

--- src/main/java/com/example/securityjpa4/demo/securityjpa4/FooBarConfig.java
+++ src/main/java/com/example/securityjpa4/demo/securityjpa4/FooBarConfig.java
@@ -1,10 +1,13 @@
 package com.example.securityjpa4.demo.securityjpa4;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class FooBarConfig extends WebSecurityConfigurerAdapter {
@@ -28,5 +31,10 @@ public class FooBarConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
