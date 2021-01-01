d30826a8fbdf60608caf6575ee743bd46571f90b
Vipul Chauhan <viratdev07@gmail.com>
2021-01-01 09:20:10 +0530

    UserDetailsService implementation

--- src/main/java/com/example/securityjpa4/demo/securityjpa4/MyUserDetailsService.java
+++ src/main/java/com/example/securityjpa4/demo/securityjpa4/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.securityjpa4.demo.securityjpa4;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-
+       return userRepository.findByUserName(s);
     }
 }
---
+++ src/main/java/com/example/securityjpa4/demo/securityjpa4/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.securityjpa4.demo.securityjpa4;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public User findByUserName(String name);
+
+}
